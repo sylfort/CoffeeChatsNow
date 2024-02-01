4ab78f9fea078b1ab94e7023b975bcbdf832e7cc
Sylvio Mafort <sylviomafort@gmail.com>
2024-02-01 14:03:07 +0000

    post prisma studio test

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { postsRouter } from "./routers/posts";
 
@@ -8,7 +7,6 @@ import { postsRouter } from "./routers/posts";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   posts: postsRouter,
 });
 
