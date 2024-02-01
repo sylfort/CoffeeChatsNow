8865d7bbd6285971d1e42e01d463a14d2d9c33e6
Sylvio Mafort <sylviomafort@gmail.com>
2024-02-01 15:49:46 +0000

    madded author to posts api

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,11 +1,35 @@
 // import { z } from "zod";
 
+import { clerkClient } from "@clerk/nextjs";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { type User } from '@clerk/nextjs/dist/types/server';
+
+const filterUserForClient = (user: User) => {
+
+  return {
+    id: user.id, 
+    username: user.username, 
+    imgUrl: user.imageUrl
+  }
+}
 
 export const postsRouter = createTRPCRouter({
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((posts) => posts.authorId),
+      limit: 100,
+    })
+    ).map(filterUserForClient)
+
+  return posts.map((post) => ({
+    post,
+    author: users.find((user) => user.id === post.authorId)
+  }))
   }),
 });
 
